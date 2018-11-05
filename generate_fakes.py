883f05cbe69ff769e6e44933aec49532d27fd0c6
aszen <aszenz@users.noreply.github.com>
2018-11-05 15:07:09 +0530

    Update generate_fakes.py

--- generate_fakes.py
+++ generate_fakes.py
@@ -2,7 +2,7 @@ import argparse
 import torch
 import torch.nn as nn
 import torchvision.utils as vutils
-from main import Generator
+from generator import Generator
 from IPython.display import Image
 
 parser = argparse.ArgumentParser()
